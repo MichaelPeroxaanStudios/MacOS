dc42512b151ece9f61f59dc8705f1f78e77b0dd4
Jason R <mail4jasonr@gmail.com>
2020-10-09 20:09:34 +0800

    Made Michael Clickable

--- Michael Peroxaan/ContentView.swift
+++ Michael Peroxaan/ContentView.swift
@@ -12,13 +12,26 @@ struct ContentView: View {
         ZStack {
             Color.init("Background")
                 .edgesIgnoringSafeArea(.all)
-            
-            Image("MichaelPeroxaan")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+            Button(action: {
+                print("button pressed")
+                ShowAlert()
+            }) {
+                Image("MichaelPeroxaan")
+                    .renderingMode(.original)
+                    .resizable()
+                    .minimumScaleFactor(1)
+                    .aspectRatio(contentMode: .fit)
+            }.buttonStyle(BorderlessButtonStyle())
         }
-
     }
+    func ShowAlert() {
+        let alert = NSAlert()
+        alert.messageText = "Michael Peroxaan"
+        alert.informativeText = "What else did you expect this app would do? This is a random app about Michael Peroxaan."
+        alert.icon = NSImage(imageLiteralResourceName: "MichaelPeroxaan")
+        alert.runModal()
+    }
+
 }
 
 
