b8f381042e53f92d137c8eb752822bf4375194fa
Jason R <mail4jasonr@gmail.com>
2020-10-09 13:23:30 +0800

    Initial Commit

---
+++ Michael Peroxaan/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Michael Peroxaan
+//
+//  Created by Jason Rosenzweig on 10/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
